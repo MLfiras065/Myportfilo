9d3ed315eff7788db96018396a5ab8443003cd78
unknown <mlayehf@gmail.com>
2024-09-07 21:00:20 +0100

    img

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,12 +5,11 @@ import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
 import {
-  TwitterIcon,
   InstagramIcon,
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/me (1).jpg'
+import test from '@/images/me (1).jpg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -50,8 +49,8 @@ export default function About() {
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
-              <Image
-                src={portraitImage}
+              <img
+                src={test}
                 alt="Firas Mlayeh portrait"
                 sizes="(min-width: 1024px) 32rem, 20rem"
                 className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
